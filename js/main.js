c441b71e76642b328aca07edb876044d484a5054
wangjue <wangjue1199@163.com>
2016-11-28 13:02:33 +0800

    ajax

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 // Start the gallery
-require(['detector', 'gallery'],
+require(['detector', 'gallery', 'jQuery'],
   function (Detector, gallery) {
     if (!Detector.webgl) {
       Detector.addGetWebGLMessage();
     }
 
     // Initialize our gallery and start the animation loop (animate is expected to call itself)
-    gallery.init();
-    gallery.animate();
-  });
+    $.ajaxSetup({
+      cache: false
+    });
+    $.getJSON("movies/meta.json", function (data) {
+      gallery.setData(data);
+
+      gallery.init();
+      gallery.buildGUI();
+      gallery.animate();
+    });
+  }
+);
